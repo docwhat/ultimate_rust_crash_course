fdc0a8b092f8df3d3bcef027d3f25778d6582f97
Christian Höltje <docwhat@gerf.org>
2021-07-31 13:56:14 -0400

    checkpoint

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,9 +1,9 @@
 const STARTING_MISSILES: i32 = 8;
-const READY_MISSILES: i32 = 2;
+const READY_AMOUNT: i32 = 2;
 
 fn main() {
-	let (mut missiles, ready) = (STARTING_MISSILES, READY_MISSILES);
-	println!("Firing {} of my {} missiles...", ready, missiles);
-	missiles = missiles - ready;
-	println!("{} missiles left", missiles - ready);
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    println!("{} missiles left", missiles - ready);
 }
